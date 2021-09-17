ac43c6c5d8665fe76681b2174253e228365bb58e
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2021-09-17 14:18:10 +0530

    Initial commit

---
+++ ascii.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("\u2500");
+	printf("\n");
+	printf("\n");
+	printf("\u2502");
+	printf("\n");
+	printf("\n");
+	printf("\u2514");
+	printf("\n");
+	printf("\n");
+	printf("\u2591");
+	printf("\u2591");
+	printf("\u2591");
+	printf("\n");
+	printf("\n");
+	return 0;
+}
