037236abba1c22009daf4734995df0500e044dd5
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2021-09-19 15:22:34 +0530

    array sorting done

--- csvtoarray.c
+++ csvtoarray.c
@@ -7,6 +7,10 @@ int printAll();
 int timeSeconds(char str[10]); 
 void deleteEntry(int pos);
 void search(char str[30], int index);
+void secToMin();
+void sortTime();
+void sortMeetings();
+void sortParticipants();
 
 typedef struct _ {
 	char name[30];
@@ -78,6 +82,10 @@ int main()
 	for(int i=0;i<ind+1;i++){
 		search(data[i].name, i);
 	}
+	secToMin();
+	//sortTime();
+	//sortParticipants();
+	sortMeetings();
 	printAll();
 	printf("%d\n",ind);
     return 0;
@@ -85,7 +93,7 @@ int main()
 
 int printAll(){
     for(int i=0; i<ind; i++){
-        printf("name->%s participants->%d seconds->%d meetings->%d\n",data[i].name,data[i].participants, data[i].seconds, data[i].meetings);
+        printf("name->%s participants->%d minutes->%d meetings->%d\n",data[i].name,data[i].participants, data[i].minutes, data[i].meetings);
     }
     printf("\n");
     return 0;
@@ -107,6 +115,7 @@ int timeSeconds(char str[10]){
 	return  atoi(array[2])+(atoi(array[1])*60)+(atoi(array[0])*3600);
 	
 }
+
 void search(char str[30], int index){
 	for (int i =index+1; i< ind+1;i++){
 		if(!strcmp(str, data[i].name)){
@@ -120,9 +129,70 @@ void search(char str[30], int index){
 	}	
 	
 }
+
 void deleteEntry(int pos){
 	for(int i=pos; i<ind; i++){
             data[i] = data[i + 1];
         } 
+	
+}
+
+void secToMin(){
+	for (int i=0;i<ind+1;i++){
+		data[i].minutes = data[i].seconds/60;
+	}
+}
+
+void sortTime(){
+    
+    data_t tempVal; // variable to gandle function
+    for(int index1=0; index1<ind+1; index1++){
+        for(int index2=index1+1; index2<ind+1; index2++){
+            if(data[index1].minutes<data[index2].minutes){
+                tempVal = data[index1];
+                data[index1]= data[index2];
+                data[index2]= tempVal;
+                
+            }
+            
+        }
+        
+    }
+	
+}
+
+void sortMeetings(){
+    
+    data_t tempVal; // variable to gandle function
+    for(int index1=0; index1<ind+1; index1++){
+        for(int index2=index1+1; index2<ind+1; index2++){
+            if(data[index1].meetings<data[index2].meetings){
+                tempVal = data[index1];
+                data[index1]= data[index2];
+                data[index2]= tempVal;
+                
+            }
+            
+        }
+        
+    }
+	
+}
+
+void sortParticipants(){
+    
+    data_t tempVal; // variable to gandle function
+    for(int index1=0; index1<ind+1; index1++){
+        for(int index2=index1+1; index2<ind+1; index2++){
+            if(data[index1].participants<data[index2].participants){
+                tempVal = data[index1];
+                data[index1]= data[index2];
+                data[index2]= tempVal;
+                
+            }
+            
+        }
+        
+    }
 	
 }
